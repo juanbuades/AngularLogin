eaeb7986eda9a0caa2ab2ab89c65d8ad3f9f8180
Juan Buades <juanbuadesavila@gmail.com>
2021-11-01 17:50:41 +0100

    validaciones de contraseñas

--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -3,8 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+//Este module se encarga de exportar el componente de registro de usuario en forma de modulo
+//para poder importarlo en los modulos de la aplicacion (app.module.ts)
 @NgModule({
     declarations: [RegisterComponent],
+    //Es importante importar FormsModule y ReactiveFormsModule para el funcionamiento del modulo
+    //ademas del commonModule que viene por defecto.
     imports: [ CommonModule, FormsModule, ReactiveFormsModule ],
     exports: [RegisterComponent],
     providers: [],
